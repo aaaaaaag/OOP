373071a5f22b559d0d8722f92bce4f1d42558099
denis <orto.fosfat@yandex.ru>
2021-03-02 13:37:39 +0300

    Update error codes

---
+++ lab_01/include/Errors.h
@@ -0,0 +1,13 @@
+//
+// Created by denis on 28.02.2021.
+//
+
+#ifndef LAB_01_OOP_ERRORS_H
+#define LAB_01_OOP_ERRORS_H
+
+#define OK 0
+#define FAIL_OPEN_FILE 1
+#define FAIL_READ_FILE_DATA 2
+
+
+#endif //LAB_01_OOP_ERRORS_H
