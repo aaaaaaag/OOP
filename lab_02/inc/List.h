d444070ecddf7c3249836bccb5b9ef30a1f68841
denis <orto.fosfat@yandex.ru>
2021-04-21 10:28:25 +0300

    finish test version of list

--- lab_02/inc/List.h
+++ lab_02/inc/List.h
@@ -20,12 +20,10 @@ class list: public ISkeletonContainer {
 public:
 
     list();
-    list(list<T> &&inList); // конструктор перемещения
+    list(list<T> &&inList) noexcept; // конструктор перемещения
     list(list<T> &inList); // конструктор копирования
     list(T *inArray, const int& size);
     list(std::initializer_list<T> inNodes);
-
-    ~list() = default;
 
     bool isEmpty() override;
     void clear() override;
@@ -41,21 +39,40 @@ public:
     listIterator<T> insert(const constListIterator<T> &iterator, const T &data);
     listIterator<T> insert(const constListIterator<T> &iterator, const list<T> &list);
 
-    T pop_front();
-    T pop_back();
+    T popFront();
+    T popBack();
 
+    T remove(const listIterator<T> &inIterator);
 
+    void reverse();
 
+    list<T> &merge(const list<T> &list);
+    list<T> &merge(const T &data);
+
+    listIterator<T> begin();
+    constListIterator<T> cbegin() const;
+
+    listIterator<T> end();
+    constListIterator<T> cend() const;
 
     list<T> &operator = (const list<T> &inList); // = копирования
-    list<T> &operator = (const list<T> &&inList); // = перемещения
+    list<T> &operator = (list<T> &&inList) noexcept; // = перемещения
+
+    list<T> &operator + (const list<T> &list);
+    list<T> &operator + (const T &data);
+
+    list<T> &operator += (const list<T> &list);
+    list<T> &operator += (const T &data);
+
+    bool operator == (const list<T> &list) const;
+    bool operator != (const list<T> &list) const;
 
 protected:
 
     std::shared_ptr<listNode<T>> getHeadNode();
     std::shared_ptr<listNode<T>> getTailNode();
-
-
+    listIterator<T> pushBack(const std::shared_ptr<listNode<T>> &node);
+    listIterator<T> pushFront(const std::shared_ptr<listNode<T>> &node);
 
 
 private:
--- lab_02/src/List.cpp
+++ lab_02/src/List.cpp
@@ -16,7 +16,7 @@ list<T>::list():
 }
 
 template <typename T>
-list<T>::list(list<T> &&inList):
+list<T>::list(list<T> &&inList) noexcept :
         m_size(inList.m_size),
         m_pHead(inList.m_pHead),
         m_pTail(inList.m_pTail)
@@ -207,6 +207,272 @@ listIterator<T> list<T>::insert(const constListIterator<T> &iterator, const list
         insertIterator = insert(iterator, (*(list.cbegin() + i)).get());
 
     return insertIterator;
+}
+
+template<typename T>
+T list<T>::popFront() {
+    if (!this->m_size)
+    {
+        //TODO error process
+    }
+
+    T data = this->m_pHead->get();
+
+    if (this->m_size == 1)
+    {
+        this->m_pHead = nullptr;
+        this->m_pHead = nullptr;
+    }
+    else
+    {
+        this->m_pHead = this->m_pHead->getNext();
+    }
+
+    this->m_size--;
+
+    return data;
+}
+
+template<typename T>
+T list<T>::popBack() {
+    if (!this->m_size)
+    {
+        //TODO error process
+    }
+
+    T data = this->m_pTail->get();
+
+    if (this->m_size == 1)
+    {
+        this->m_pHead = nullptr;
+        this->m_pTail = nullptr;
+    }
+    else
+    {
+        std::shared_ptr<listNode<T>> tmpNode = this->head;
+        for (; tmpNode->getNext() != this->tail; tmpNode = tmpNode->getNext());
+
+        tmpNode->setNull();
+        this->m_pTail = tmpNode;
+        this->m_pTail->setNull();
+    }
+
+    this->m_size--;
+
+    return data;
+}
+
+template<typename T>
+T list<T>::remove(const listIterator<T> &inIterator) {
+    if (inIterator.isInvalid())
+    {
+        //TODO error process
+    }
+
+    if (!this->m_size)
+    {
+        //TODO error process
+    }
+
+    if (inIterator == this->begin())
+    {
+        return popFront();
+    }
+
+    listIterator<T> tmpIterator = this->begin();
+    for (; tmpIterator + 1 != inIterator; tmpIterator++);
+
+    T data = tmpIterator->get();
+    tmpIterator->setNext(tmpIterator->getNext()->getNext());
+    this->m_size--;
+
+    return data;
+}
+
+template<typename T>
+void list<T>::reverse() {
+    std::shared_ptr<listNode<T>> current(this->m_pHead);
+    std::shared_ptr<listNode<T>> next(nullptr);
+    std::shared_ptr<listNode<T>> prev(nullptr);
+
+    while (current)
+    {
+        next = current->getNext();
+        current->setNext(prev);
+        prev = current;
+        current = next;
+    }
+
+    prev = this->m_pHead;
+    this->m_pHead = this->m_pTail;
+    this->m_pTail = prev;
+    this->m_pTail->setNull();
+}
+
+template<typename T>
+list<T> &list<T>::operator=(const list<T> &inList) {
+    clear();
+
+    this->m_size = 0;
+    this->m_pHead = nullptr;
+    this->m_pTail = nullptr;
+
+    pushBack(inList);
+    return *this;
+}
+
+template<typename T>
+list<T> &list<T>::operator=(list<T> &&inList)  noexcept {
+    this->m_size = inList.m_size;
+    this->m_pHead = inList.m_pHead;
+    this->m_pTail = inList.m_pTail;
+}
+
+template<typename T>
+list<T> &list<T>::merge(const list<T> &list) {
+    this->pushBack(list);
+    return *this;
+}
+
+template<typename T>
+list<T> &list<T>::merge(const T &data) {
+    this->pushBack(data);
+    return *this;
+}
+
+template<typename T>
+list<T> &list<T>::operator + (const list<T> &list) {
+    this->pushBack(list);
+    return *this;
+}
+
+template<typename T>
+list<T> &list<T>::operator + (const T &data) {
+    this->pushBack(data);
+    return *this;
+}
+
+template<typename T>
+list<T> &list<T>::operator+=(const list <T> &list) {
+    this->pushBack(list);
+    return *this;
+}
+
+template<typename T>
+list<T> &list<T>::operator+=(const T &data) {
+    this->pushBack(data);
+    return *this;
+}
+
+template<typename T>
+bool list<T>::operator==(const list <T> &list) const {
+    auto fst = this->cbegin();
+    auto snd = list.cbegin();
+
+    for (; fst != this->cend() && snd != list.cend(); ++fst, ++snd)
+    {
+        if (fst->get() != snd->get())
+            return false;
+    }
+
+    return this->m_size == list.m_size;
+}
+
+template<typename T>
+bool list<T>::operator!=(const list <T> &list) const {
+    return !(*this == list);
+}
+
+template<typename T>
+listIterator<T> list<T>::begin() {
+    listIterator<T> iterator(this-m_pHead);
+    return iterator;
+}
+
+template<typename T>
+constListIterator<T> list<T>::cbegin() const {
+    constListIterator<T> iterator(this->m_pHead);
+    return iterator;
+}
+
+template<typename T>
+listIterator <T> list<T>::end() {
+    listIterator<T> iterator(this->m_pTail);
+    return ++iterator;
+}
+
+template<typename T>
+constListIterator<T> list<T>::cend() const {
+    constListIterator<T> iterator(this->m_pTail);
+    return ++iterator;
+}
+
+template<typename T>
+std::shared_ptr<listNode<T>> list<T>::getHeadNode() {
+    return this->m_pHead;
+}
+
+template<typename T>
+std::shared_ptr<listNode<T>> list<T>::getTailNode() {
+    return this->m_pTail;
+}
+
+template<typename T>
+listIterator<T> list<T>::pushBack(const std::shared_ptr<listNode<T>> &node) {
+    if (!node)
+    {
+        // TODO error process
+    }
+
+    std::shared_ptr<listNode<T>> tmpNode = nullptr;
+
+    try
+    {
+        tmpNode = std::shared_ptr<listNode<T>>(new listNode<T>);
+    }
+    catch (std::bad_alloc &error)
+    {
+        //TODO error process
+    }
+
+    tmpNode->set(node->get());
+
+    if (!this->m_size)
+    {
+        this->m_pHead = tmpNode;
+        this->m_pTail = tmpNode;
+    }
+    else
+    {
+        this->m_pTail->setNextNode(tmpNode);
+        this->m_pTail = tmpNode;
+    }
+
+    this->m_size++;
+
+    listIterator<T> iterator(this->m_pTail);
+    return iterator;
+}
+
+template<typename T>
+listIterator<T> list<T>::pushFront(const std::shared_ptr<listNode<T>> &node) {
+    if (!node)
+    {
+        //TODO error process
+    }
+
+    node->setNextNode(this->m_pHead);
+    this->m_pHead = node;
+
+    if (!this->m_size)
+    {
+        this->m_pTail = this->m_pHead;
+    }
+
+    this->m_size++;
+
+    listIterator<T> iterator(node);
+    return iterator;
 }
 
 
